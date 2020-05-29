4b69379dfae226fdef612fc4ea981958eca6667f
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-05-29 10:05:53 +0200

    simpler lock test

--- tests/test_filelock.py
+++ tests/test_filelock.py
@@ -18,9 +18,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import subprocess
 import tempfile
+import threading
+import time
 from pathlib import Path
+
+from singularity_permanent_cache import SimpleUnixFileLock
 
 
 def test_filelock():
@@ -29,19 +32,24 @@ def test_filelock():
     _, execution_times_file = tempfile.mkstemp()
     Path(execution_times_file).write_text("")  # make sure the file exists
 
-    # this script uses the implemented filelock. It waits one second
-    # after acquiring the lock and then writes the current time (in seconds) to
-    # a file.
-    filelockscript = Path(__file__).parent / "lockscript.py"
+    def filelock_test():
+        with SimpleUnixFileLock(lockfile):
+            # We wait 0.1 second to make sure current time is distinct if the
+            # lock works.
+            time.sleep(0.1)
+            current_time = str(round(time.time(), 1))
+            # Use append mode here
+            with open(execution_times_file, mode="at") as file_h:
+                file_h.write(current_time + "\n")
 
     # Start all the processes in quick succession.
-    processes = [subprocess.Popen([str(filelockscript), lockfile,
-                                   execution_times_file])
-                 for _ in range(count)]
+    threads = [threading.Thread(target=filelock_test) for _ in range(count)]
 
-    # Wait on all processes to finish.
-    for process in processes:
-        process.wait()
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
 
     # Get the epoch time (in seconds) from the file. If the lock does not work
     # We expect multiple processes to have executed at the same time.
